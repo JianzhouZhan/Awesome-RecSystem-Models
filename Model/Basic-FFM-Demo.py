43bdc9b09a8b0364d283452cae33ec7d1a8730b2
STU14jzzhan <jianzhouzhan@126.com>
2019-09-10 20:29:40 +0800

    1.初始化FFM模型Demo 2.优化FM模型(使用L1, L2正则化来避免参数过大) 3.移动一些共用的函数至Util

--- Model/Basic-FFM-Demo.py
+++ Model/Basic-FFM-Demo.py
@@ -3,10 +3,10 @@ import numpy as np
 import pandas as pd
 import torch.nn.functional as F
 import torch.nn as nn
-from sklearn import preprocessing
 from util.load_data_util import get_batch_loader
+from sklearn import preprocessing
 
-EPOCHS = 500
+EPOCHS = 1000
 BATCH_SIZE = 1000
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -15,6 +15,11 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 """                                      数据读取和转换                                    """
 """ ************************************************************************************ """
 def load_dataset():
+    """
+    FFM模型里Load数据的方式与FM模型中基本一致, 除了Field部分
+    :return:
+    """
+    # Step1: 获取基本信息
     header = ['user_id', 'age', 'gender', 'occupation', 'zip_code']
     df_user = pd.read_csv('../data/FM-Data/u.user', sep='|', names=header)
     header = ['item_id', 'title', 'release_date', 'video_release_date', 'IMDb_URL', 'unknown', 'Action', 'Adventure',
@@ -32,7 +37,25 @@ def load_dataset():
     user_features = df_user.columns.values.tolist()
     movie_features = df_item.columns.values.tolist()
     cols = user_features + movie_features
+    cols.remove('user_id')
+    cols.remove('item_id')
 
+    # Step2: 把特征进行归类处理(分成4个field)
+    # 这里, 如果我们把Field分成4类, Gender, Occupation, Age, Other
+    field_index, feature2field = {}, {}
+    other_idxs = []
+    for idx, col in enumerate(cols):
+        infos = col.split('_')
+        if len(infos) == 2:
+            field = infos[0]
+            field_index[field] = field_index.get(field, len(field_index))
+            feature2field[idx] = field_index[field]
+        if len(infos) == 1:
+            other_idxs.append(idx)
+    for idx in other_idxs:
+        feature2field[idx] = len(field_index)
+
+    # Step3: 根据user_id, item_id进行Merge, 得到对应的数据
     header = ['user_id', 'item_id', 'rating', 'timestamp']
     df_train = pd.read_csv('../data/FM-Data/ua.base', sep='\t', names=header)
     df_train = df_train.merge(df_user, on='user_id', how='left')
@@ -42,6 +65,7 @@ def load_dataset():
     df_test = df_test.merge(df_user, on='user_id', how='left')
     df_test = df_test.merge(df_item, on='item_id', how='left')
 
+    # Step4: Label的变换, 以避免Cuda中报错
     # 需要对Label进行一定的转换, 因为原始的Label是[1, 2, 3, 4, 5]
     # 而 cuda中, 如果直接以这种Label的话, 会报错(Label 需要在[0, n_class - 1]范围
     # 因此, 需要转成[0, 1, 2, 3, 4]
@@ -53,74 +77,76 @@ def load_dataset():
     df_train['rating'] = df_train.rating.apply(lambda x: map_dict[x])
     df_test['rating'] = df_test.rating.apply(lambda x: map_dict[x])
 
-    # 如果想要使用"二分类"的话, 可以使用下面的方式来处理
+    # # 如果想要使用"二分类"的话, 可以使用下面的方式来处理
     # df_train['rating'] = df_train.rating.apply(lambda x: 1 if int(x) == 1 else 0)
     # df_test['rating'] = df_test.rating.apply(lambda x: 1 if int(x) == 1 else 0)
 
+    # Step5: 输出成np.array格式
     train_labels = np.array(df_train['rating'].astype(np.int32))
     test_labels = np.array(df_test['rating'].astype(np.int32))
-    return df_train[cols].values, train_labels, df_test[cols].values, test_labels
+    return df_train[cols].values, train_labels, df_test[cols].values, test_labels, feature2field
 
 
 """ ************************************************************************************ """
-"""                                          FM层                                        """
+"""                                          FFM层                                       """
 """ ************************************************************************************ """
-class FM_layer(nn.Module):
-    def __init__(self, reg_l1=0.01, reg_l2=0.01, class_num=1, feature_num=10, latent_factor_dim=5):
-        super(FM_layer, self).__init__()
+class FFM_layer(nn.Module):
+    def __init__(self, field_map_dict, fea_num, reg_l1=0.01, reg_l2=0.01, class_num=1, latent_factor_dim=10):
+        super(FFM_layer, self).__init__()
         self.reg_l1 = reg_l1
         self.reg_l2 = reg_l2
-        self.fea_num = feature_num
-        self.k = latent_factor_dim
-        self.class_num = class_num
-        self.linear = nn.Linear(self.fea_num, class_num)  # 前两项的线性层
-        self.v = nn.Parameter(torch.randn(self.fea_num, self.k, class_num))  # 交互矩阵
+        self.fea_num = fea_num
+        self.field_map_dict = field_map_dict  # 需要有个下标对应的字典
+
+        field_num = len(field_map_dict)
+        self.linear = nn.Linear(fea_num, class_num)   # 用于bias与线性部分
+        self.v = nn.Parameter(torch.randn(fea_num, field_num, latent_factor_dim, class_num))
 
     def forward(self, x):
         # 先计算得到线性的那一部分
         linear_part = self.linear(x)
 
-        # 再计算得到交互的那一个部分
-        # 为了使用矩阵计算, (Batch * fea_num) * (fea_num * k * class_num), 我们使用Tensor的转置来处理
-        interaction_part1 = torch.matmul(self.v.permute(2, 1, 0), x.T).permute(2, 1, 0)
-        interaction_part1 = torch.pow(interaction_part1, 2)
-        interaction_part1 = -0.5 * torch.sum(interaction_part1, dim=1)
-        interaction_part1 = torch.squeeze(interaction_part1, dim=1)
+        # 计算交叉部分
+        interaction_part = 0.0
+        for i in range(self.fea_num):
+            for j in range(i + 1, self.fea_num):
+                v_ifj = self.v[i, self.field_map_dict[j], :, :]
+                v_jfi = self.v[j, self.field_map_dict[i], :, :]
 
-        x_square, v_square = torch.pow(x, 2), torch.pow(self.v, 2)
-        interaction_part2 = torch.matmul(v_square.permute(2, 1, 0), x_square.T).permute(2, 1, 0)
-        interaction_part2 = 0.5 * torch.sum(interaction_part2, dim=1)
-        interaction_part2 = torch.squeeze(interaction_part2, dim=1)
+                xij = torch.unsqueeze(x[:, i] * x[:, j], dim=1)
+                v_ijji = torch.unsqueeze(torch.sum(v_ifj * v_jfi, dim=0), dim=0)
 
-        output = linear_part + interaction_part1 + interaction_part2
+                interaction_part += torch.mm(xij, v_ijji)
 
-        output = F.log_softmax(output, dim=1)
+        output = linear_part + interaction_part
+        output = torch.log_softmax(output, dim=1)
         return output
 
 
 """ ************************************************************************************ """
 """                                     训练和测试FM模型                                   """
 """ ************************************************************************************ """
-def train_FM_model_demo():
+def train_FFM_model_demo():
 
     # Step1: 导入数据
-    x_train, y_train, x_test, y_test = load_dataset()
+    x_train, y_train, x_test, y_test, feature2field = load_dataset()
     x_train = preprocessing.scale(x_train, with_mean=True, with_std=True)
     x_test = preprocessing.scale(x_test, with_mean=True, with_std=True)
     class_num = len(set([y for y in y_train] + [y for y in y_test]))
 
-    # FM模型
-    fm = FM_layer(class_num=class_num, feature_num=x_train.shape[1], latent_factor_dim=40).to(DEVICE)
+    # FFM模型
+    ffm = FFM_layer(field_map_dict=feature2field, fea_num=x_train.shape[1], reg_l1=0.01, reg_l2=0.01,
+                    class_num=class_num, latent_factor_dim=10).to(DEVICE)
 
     # 定义损失函数还有优化器
-    optm = torch.optim.Adam(fm.parameters())
+    optm = torch.optim.Adam(ffm.parameters())
 
     train_loader = get_batch_loader(x_train, y_train, BATCH_SIZE, shuffle=True)
     test_loader = get_batch_loader(x_test, y_test, BATCH_SIZE, shuffle=False)
 
     for epoch in range(1, EPOCHS + 1):
-        train(fm, DEVICE, train_loader, optm, epoch)
-        test(fm, DEVICE, test_loader)
+        train(ffm, DEVICE, train_loader, optm, epoch)
+        test(ffm, DEVICE, test_loader)
 
 
 def train(model, device, train_loader, optimizer, epoch):
@@ -129,13 +155,8 @@ def train(model, device, train_loader, optimizer, epoch):
         data, target = data.to(device, dtype=torch.float32), target.to(device).long()
         optimizer.zero_grad()
         output = model(data)
+        loss = F.nll_loss(output, target)
 
-        if model.class_num == 2:
-            loss = F.cross_entropy(output, target)
-        else:
-            loss = F.nll_loss(output, target)
-
-        # 加上L1和L2损失
         regularization_loss = 0
         for param in model.parameters():
             regularization_loss += model.reg_l1 * torch.sum(torch.abs(param))
@@ -150,18 +171,16 @@ def train(model, device, train_loader, optimizer, epoch):
                 100. * batch_idx / len(train_loader), loss.item()
             ))
 
+
 def test(model, device, test_loader):
     model.eval()
-    test_loss, correct = 0, 0
+    test_loss = 0
+    correct = 0
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device, dtype=torch.float32), target.to(device).long()
             output = model(data)
-
-            if model.class_num == 2:
-                test_loss += F.cross_entropy(output, target)
-            else:
-                test_loss += F.nll_loss(output, target, reduction='sum').item()
+            test_loss += F.nll_loss(output, target, reduction='sum').item()
 
             regularization_loss = 0
             for param in model.parameters():
@@ -178,4 +197,4 @@ def test(model, device, test_loader):
 
 
 if __name__ == '__main__':
-    train_FM_model_demo()
+    train_FFM_model_demo()
---
+++ util/load_data_util.py
@@ -0,0 +1,19 @@
+import torch.utils.data as data
+
+
+def get_batch_loader(features, labels, batch_size, shuffle=True):
+    class MyDataset(data.Dataset):
+        def __init__(self, features, labels):
+            self.features = features
+            self.labels = labels
+
+        def __getitem__(self, index):  # 返回的是tensor
+            row_data, target = self.features[index], self.labels[index]
+            return row_data, target
+
+        def __len__(self):
+            return len(self.features)
+
+    batch_loader = data.DataLoader(MyDataset(features, labels), batch_size=batch_size, shuffle=shuffle)
+    return batch_loader
+
