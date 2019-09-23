5b80608cb98af3c644d615528606da09433f33fc
STU14jzzhan <jianzhouzhan@126.com>
2019-09-23 18:37:58 +0800

    1. 优化DCN网络算法(批量读取数据的方式)

--- Model/Basic-DCN-Demo.py
+++ Model/Basic-DCN-Demo.py
@@ -1,19 +1,30 @@
 import re
 import os
 import math
-import pickle
 import torch
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 from sklearn.metrics import roc_auc_score
-from data.Criteo.DCN_dataProcess import EACH_FILE_DATA_NUM
 from time import time
 
-EPOCHS = 100
-BATCH_SIZE = 1024
+EPOCHS = 5
+BATCH_SIZE = 2048
+AID_DATA_DIR = '../data/Criteo/forDCN/'  # 辅助用途的文件路径
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
+"""
+Pytorch implementation of Deep & Cross Network[1]
+
+Reference:
+[1] Deep & Cross Network for Ad Click Predictions,
+    Ruoxi Wang, Bin Fu, Gang Fu, Zhenguo Li, Mingliang Wang
+[2] Keras implementation of Deep & Cross Network
+    https://github.com/Nirvanada/Deep-and-Cross-Keras 
+[3] PaddlePaddle implemantation of Deep & Cross Network
+    https://github.com/PaddlePaddle/models/tree/develop/PaddleRec/ctr/dcn
+"""
 
 class DCN_layer(nn.Module):
     def __init__(self, num_dense_feat, num_sparse_feat_list, dropout_deep, deep_layer_sizes,
@@ -63,8 +74,8 @@ class DCN_layer(nn.Module):
     def forward(self, feat_index_list, dense_x):
         x0 = dense_x
         for i, feat_index in enumerate(feat_index_list):
-            sparse_x = self.sparse_feat_embeddings[i](feat_index)                                     # None * F * K
-            x0 = torch.cat((x0, sparse_x), dim=1)                                             # None * In
+            sparse_x = self.sparse_feat_embeddings[i](feat_index)
+            x0 = torch.cat((x0, sparse_x), dim=1)                             # None * In
 
         # Cross Network 部分
         x_cross = x0                                                          # None * In
@@ -96,21 +107,29 @@ def train_DeepFM_model_demo(device):
     训练DeepFM的方式
     :return:
     """
-    train_filelist = ["%s%s" % ('../data/Criteo/train_data/', x) for x in os.listdir('../data/Criteo/train_data/')]
-    test_filelist = ["%s%s" % ('../data/Criteo/test_data/', x) for x in os.listdir('../data/Criteo/test_data/')]
+    train_filelist = ["%s%s" % (AID_DATA_DIR + 'train/', x) for x in os.listdir(AID_DATA_DIR + 'train/')]
+    test_filelist = ["%s%s" % (AID_DATA_DIR + 'test_valid/', x) for x in os.listdir(AID_DATA_DIR + 'test_valid/')]
+
     train_file_id = [int(re.sub('[\D]', '', x)) for x in train_filelist]
     train_filelist = [train_filelist[idx] for idx in np.argsort(train_file_id)]
 
     test_file_id = [int(re.sub('[\D]', '', x)) for x in test_filelist]
     test_filelist = [test_filelist[idx] for idx in np.argsort(test_file_id)]
 
-    feat_dict_ = pickle.load(open('../data/Criteo/aid_data/DCN_feat_dict_10.pkl2', 'rb'))
-    num_sparse_feat_list = [len(v) + 1 for k, v in feat_dict_.items()]
+    num_sparse_feat_list = []
+    for line in open(AID_DATA_DIR + 'cat_feature_num.txt'):
+        cat_feat_num = line.rstrip().split(' ')
+        num_sparse_feat_list.append(int(cat_feat_num[1]) + 1)
+
+    # num_sparse_feat_list = []
+    # with open(fname.strip(), 'r') as fin:
+    #     for line in fin:
+    #         cat_feat_num = line.rstrip().split(' ')
+    #         num_sparse_feat_list.append(int(cat_feat_num[1]) + 1)
 
     # 下面的num_sparse_feat之所以还要加1个维度, 是因为缺失值的处理(详见数据处理过程)
     dcn = DCN_layer(reg_l2=1e-5, num_dense_feat=13, num_sparse_feat_list=num_sparse_feat_list,
-                    dropout_deep=[0.5, 0.5, 0.5], deep_layer_sizes=[1024, 1024],
-                    num_cross_layers=6).to(DEVICE)
+                    dropout_deep=[0.5, 0.5, 0.5], deep_layer_sizes=[1024, 1024], num_cross_layers=6).to(DEVICE)
     print("Start Training DeepFM Model!")
 
     # 定义损失函数还有优化器
@@ -129,61 +148,77 @@ def train_DeepFM_model_demo(device):
                 test_item_count += 1
 
     # 由于数据量过大, 如果使用pytorch的DataSet来自定义数据的话, 会耗时很久, 因此, 这里使用其它方式
+    cat_feat_idx_dict_list = [{} for _ in range(26)]
+    for i in range(26):
+        lookup_idx = 1  # remain 0 for default value
+        for line in open(os.path.join(AID_DATA_DIR + 'vocab', 'C' + str(i + 1) + '.txt')):
+            cat_feat_idx_dict_list[i][line.strip()] = lookup_idx
+            lookup_idx += 1
+
     for epoch in range(1, EPOCHS + 1):
         tic = time()
-        train(dcn, train_filelist, train_item_count, feat_dict_, device, optimizer, epoch)
+        train(dcn, train_filelist, train_item_count, device, optimizer, epoch, cat_feat_idx_dict_list)
+        torch.save(dcn, 'DCN_' + str(epoch) + '.model')
         toc = time()
-        test(dcn, test_filelist, test_item_count, feat_dict_, device)
+        dcn1 = torch.load('DCN_' + str(epoch) + '.model')
+        dcn1.eval()
+        test(dcn1, test_filelist, test_item_count, device, cat_feat_idx_dict_list)
         print('The Time of Epoch: %.5f min' % float((toc - tic) / 60.0))
         print('The Test Time of Epoch: %.5f min' % float((time() - toc) / 60.0))
 
 
-def test(model, test_filelist, test_item_count, feat_dict_, device):
+def test(model, test_filelist, test_item_count, device, cat_feat_idx_dict_list):
     fname_idx = 0
     pred_y, true_y = [], []
-    fsparse_features_idxs, dense_features_values, labels = None, None, None
+    sparse_features_idxs, dense_features_values, labels = None, None, None
     test_loss = 0
     with torch.no_grad():
         # 不断地取出数据进行计算
+        pre_file_data_count = 0
         for batch_idx in range(math.ceil(test_item_count / BATCH_SIZE)):
             # 取出当前Batch所在的数据的下标
             st_idx, ed_idx = batch_idx * BATCH_SIZE, (batch_idx + 1) * BATCH_SIZE
             ed_idx = min(ed_idx, test_item_count - 1)
 
-            if fsparse_features_idxs is None:
-                fsparse_features_idxs, dense_features_values, labels = get_idx_value_label(
-                    test_filelist[fname_idx], feat_dict_, shuffle=False)
+            if sparse_features_idxs is None:
+                # sparse_features_idxs, dense_features_values, labels = get_idx_value_label(
+                #     test_filelist[fname_idx], feat_dict_, shuffle=False)
+                sparse_features_idxs, dense_features_values, labels = new_get_idx_value_label(
+                    test_filelist[fname_idx], cat_feat_idx_dict_list, shuffle=False)
 
-            st_idx = st_idx - fname_idx * EACH_FILE_DATA_NUM
-            ed_idx = ed_idx - fname_idx * EACH_FILE_DATA_NUM
+            st_idx -= pre_file_data_count
+            ed_idx -= pre_file_data_count
 
-            if ed_idx < EACH_FILE_DATA_NUM:
-                batch_fea_idxs = fsparse_features_idxs[st_idx:ed_idx, :]
+            if ed_idx <= len(sparse_features_idxs):
+                batch_fea_idxs = sparse_features_idxs[st_idx:ed_idx, :]
                 batch_fea_values = dense_features_values[st_idx:ed_idx, :]
                 batch_labels = labels[st_idx:ed_idx, :]
             else:
-                batch_fea_idxs_part1 = fsparse_features_idxs[st_idx:ed_idx, :]
-                batch_fea_values_part1 = dense_features_values[st_idx:ed_idx, :]
-                batch_labels_part1 = labels[st_idx:ed_idx, :]
+                pre_file_data_count += len(sparse_features_idxs)
 
+                batch_fea_idxs_part1 = sparse_features_idxs[st_idx::, :]
+                batch_fea_values_part1 = dense_features_values[st_idx::, :]
+                batch_labels_part1 = labels[st_idx::, :]
+
+                ed_idx -= len(sparse_features_idxs)
                 fname_idx += 1
-                sparse_features_idxs, dense_features_values, labels = get_idx_value_label(
-                    test_filelist[fname_idx], feat_dict_, shuffle=False)
-                ed_idx = ed_idx - EACH_FILE_DATA_NUM
+                # sparse_features_idxs, dense_features_values, labels = get_idx_value_label(
+                #     test_filelist[fname_idx], feat_dict_, shuffle=False)
+                sparse_features_idxs, dense_features_values, labels = new_get_idx_value_label(
+                    test_filelist[fname_idx], cat_feat_idx_dict_list, shuffle=False)
 
-                batch_fea_idxs_part2 = fsparse_features_idxs[0:ed_idx, :]
+                batch_fea_idxs_part2 = sparse_features_idxs[0:ed_idx, :]
                 batch_fea_values_part2 = dense_features_values[0:ed_idx, :]
                 batch_labels_part2 = labels[0:ed_idx, :]
 
                 batch_fea_idxs = np.vstack((batch_fea_idxs_part1, batch_fea_idxs_part2))
                 batch_fea_values = np.vstack((batch_fea_values_part1, batch_fea_values_part2))
                 batch_labels = np.vstack((batch_labels_part1, batch_labels_part2))
-
             batch_fea_values = torch.from_numpy(batch_fea_values)
             batch_labels = torch.from_numpy(batch_labels)
 
             sparse_idx_list = []
-            for i in range(len(feat_dict_)):
+            for i in range(26):
                 sparse_idx = batch_fea_idxs[:, i]
                 sparse_idx = torch.LongTensor([int(x) for x in sparse_idx])
                 sparse_idx = sparse_idx.to(device)
@@ -203,35 +238,42 @@ def test(model, test_filelist, test_item_count, feat_dict_, device):
         print('Test set: Average loss: {:.5f}'.format(test_loss))
 
 
-def train(model, train_filelist, train_item_count, feat_dict_, device, optimizer, epoch):
+def train(model, train_filelist, train_item_count, device, optimizer, epoch, cat_feat_idx_dict_list):
     fname_idx = 0
     sparse_features_idxs, dense_features_values, labels = None, None, None
     # 依顺序来遍历访问
+    pre_file_data_count = 0
     for batch_idx in range(math.ceil(train_item_count / BATCH_SIZE)):
         # 得到当前Batch所在的数据的下标
         st_idx, ed_idx = batch_idx * BATCH_SIZE, (batch_idx + 1) * BATCH_SIZE
         ed_idx = min(ed_idx, train_item_count - 1)
 
         if sparse_features_idxs is None:
-            sparse_features_idxs, dense_features_values, labels = get_idx_value_label(
-                train_filelist[fname_idx], feat_dict_)
+            # sparse_features_idxs, dense_features_values, labels = get_idx_value_label(
+            #     train_filelist[fname_idx], feat_dict_)
+            sparse_features_idxs, dense_features_values, labels = new_get_idx_value_label(
+                train_filelist[fname_idx], cat_feat_idx_dict_list, shuffle=True)
 
-        st_idx = st_idx - fname_idx * EACH_FILE_DATA_NUM
-        ed_idx = ed_idx - fname_idx * EACH_FILE_DATA_NUM
+        st_idx -= pre_file_data_count
+        ed_idx -= pre_file_data_count
 
-        if ed_idx < EACH_FILE_DATA_NUM:
+        if ed_idx < len(sparse_features_idxs):
             batch_fea_idxs = sparse_features_idxs[st_idx:ed_idx, :]
             batch_fea_values = dense_features_values[st_idx:ed_idx, :]
             batch_labels = labels[st_idx:ed_idx, :]
         else:
-            batch_fea_idxs_part1 = sparse_features_idxs[st_idx:ed_idx, :]
-            batch_fea_values_part1 = dense_features_values[st_idx:ed_idx, :]
-            batch_labels_part1 = labels[st_idx:ed_idx, :]
+            pre_file_data_count += len(sparse_features_idxs)
 
+            batch_fea_idxs_part1 = sparse_features_idxs[st_idx::, :]
+            batch_fea_values_part1 = dense_features_values[st_idx::, :]
+            batch_labels_part1 = labels[st_idx::, :]
+
+            ed_idx -= len(sparse_features_idxs)
             fname_idx += 1
-            ed_idx = ed_idx - EACH_FILE_DATA_NUM
-            sparse_features_idxs, dense_features_values, labels = get_idx_value_label(
-                train_filelist[fname_idx], feat_dict_)
+            # sparse_features_idxs, dense_features_values, labels = get_idx_value_label(
+            #     train_filelist[fname_idx], feat_dict_)
+            sparse_features_idxs, dense_features_values, labels = new_get_idx_value_label(
+                train_filelist[fname_idx], cat_feat_idx_dict_list, shuffle=True)
 
             batch_fea_idxs_part2 = sparse_features_idxs[0:ed_idx, :]
             batch_fea_values_part2 = dense_features_values[0:ed_idx, :]
@@ -245,7 +287,7 @@ def train(model, train_filelist, train_item_count, feat_dict_, device, optimizer
         batch_labels = torch.from_numpy(batch_labels)
 
         sparse_idx_list = []
-        for i in range(len(feat_dict_)):
+        for i in range(26):
             sparse_idx = batch_fea_idxs[:, i]
             sparse_idx = torch.LongTensor([int(x) for x in sparse_idx])
             sparse_idx = sparse_idx.to(device)
@@ -264,11 +306,60 @@ def train(model, train_filelist, train_item_count, feat_dict_, device, optimizer
         loss += regularization_loss
 
         loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=100)
         optimizer.step()
         if batch_idx % 1000 == 0:
-            print('Train Epoch: {} [{} / {} ({:.0f})%]\tLoss:{:.6f}'.format(
-                epoch, batch_idx * len(sparse_idx), train_item_count,
+            print('Train Epoch: {} [{} / {} ({:.2f}%)]\tLoss:{:.6f}'.format(
+                epoch, batch_idx * BATCH_SIZE, train_item_count,
                 100. * batch_idx / math.ceil(int(train_item_count / BATCH_SIZE)), loss.item()))
+
+
+def new_get_idx_value_label(fname, cat_feat_idx_dict_list, shuffle=True):
+    cont_idx_ = list(range(1, 14))
+    cat_idx_ = list(range(14, 40))
+
+    def new_process_line(line):
+        sparse_feat_idx = []
+        dense_feat_value = []
+
+        features = line.rstrip('\n').split('\t')
+        for idx in cont_idx_:
+            if features[idx] == '':
+                dense_feat_value.append(0)
+            else:
+                # log transform
+                dense_feat_value.append(
+                    math.log(4 + float(features[idx])) if idx == 2 else math.log(1 + float(features[idx])))
+
+        for idx in cat_idx_:
+            if features[idx] == '' or features[idx] not in cat_feat_idx_dict_list[idx - 14]:
+                sparse_feat_idx.append(0)
+            else:
+                sparse_feat_idx.append(cat_feat_idx_dict_list[idx - 14][features[idx]])
+
+        return sparse_feat_idx, dense_feat_value, [int(features[0])]
+
+    sparse_features_idxs, dense_features_values, labels = [], [], []
+    with open(fname.strip(), 'r') as fin:
+        for line in fin:
+            sparse_feat_idx, dense_feat_value, label = new_process_line(line)
+            sparse_features_idxs.append(sparse_feat_idx)
+            dense_features_values.append(dense_feat_value)
+            labels.append(label)
+
+    sparse_features_idxs = np.array(sparse_features_idxs)
+    dense_features_values = np.array(dense_features_values)
+    labels = np.array(labels).astype(np.int32)
+
+    # 进行shuffle
+    if shuffle:
+        idx_list = np.arange(len(labels))
+        np.random.shuffle(idx_list)
+        sparse_features_idxs = sparse_features_idxs[idx_list, :]
+        dense_features_values = dense_features_values[idx_list, :]
+        labels = labels[idx_list, :]
+    return sparse_features_idxs, dense_features_values, labels
+
 
 def get_idx_value_label(fname, feat_dict_, shuffle=True):
     continuous_range_ = range(1, 14)
