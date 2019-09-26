e2f60fcb2ade5c113b0171af8967762ec18bd802
STU14jzzhan <jianzhouzhan@126.com>
2019-09-26 14:15:40 +0800

    1. Comment some not used methods.

--- data/Criteo/forDeepFM/deepFM_dataProcess.py
+++ data/Criteo/forDeepFM/deepFM_dataProcess.py
@@ -22,7 +22,7 @@ def get_raw_data():
     fin = open('../train.txt', 'r')
     fout = open('raw_data/part-0', 'w')
     for line_idx, line in enumerate(fin):
-        # # 生成小样本, 用于测试算法
+        # get mini-sample for test
         # if line_idx >= EACH_FILE_DATA_NUM * 10:
         #     break
 
@@ -70,7 +70,7 @@ def get_feat_dict():
         feat_cnt = Counter()
         with open('../train.txt', 'r') as fin:
             for line_idx, line in enumerate(fin):
-                # # 生成小样本, 用于测试算法
+                # for test
                 # if line_idx >= EACH_FILE_DATA_NUM * 10:
                 #     break
 
@@ -98,7 +98,7 @@ def get_feat_dict():
         cnt_feat_set = set()
         with open('../train.txt', 'r') as fin:
             for line_idx, line in enumerate(fin):
-                # # 生成小样本, 用于测试算法
+                # get mini-sample for test
                 # if line_idx >= EACH_FILE_DATA_NUM * 10:
                 #     break
 
@@ -117,125 +117,124 @@ def get_feat_dict():
         print('args.num_feat ', len(feat_dict) + 1)
 
 
-""" ************************************************************************************ """
-"""                                     下面两个方法已经弃用                                """
-""" ************************************************************************************ """
-def get_criteo_data(filelist):
-    """
-    获取文件目录下的Criteo数据集
-    :param filelist:
-    :return:
-    """
-    continuous_range_ = range(1, 14)
-    categorical_range_ = range(14, 40)
-    cont_min_ = [0, -3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    cont_max_ = [5775, 257675, 65535, 969, 23159456, 431037, 56311, 6047, 29019, 46, 231, 4008, 7393]
-    cont_diff_ = [cont_max_[i] - cont_min_[i] for i in range(len(cont_min_))]
-    print(os.getcwd())
-    feat_dict_ = pickle.load(open('data/aid_data/feat_dict_10.pkl2', 'rb'))
-
-    count = 0
-    features_idxs, features_values, labels = [], [], []
-    for fname in filelist:
-
-        count += 1
-        print(count)
-
-        with open(fname.strip(), 'r') as fin:
-            for line in fin:
-                features = line.rstrip('\n').split('\t')
-                feat_idx = []
-                feat_value = []
-
-                # MinMax标准化连续型数据
-                for idx in continuous_range_:
-                    if features[idx] == '':
-                        feat_idx.append(0)
-                        feat_value.append(0.0)
-                    else:
-                        feat_idx.append(feat_dict_[idx])
-                        feat_value.append((float(features[idx]) - cont_min_[idx - 1]) / cont_diff_[idx - 1])
-
-                #
-                for idx in categorical_range_:
-                    if features[idx] == '' or features[idx] not in feat_dict_:
-                        feat_idx.append(0)
-                        feat_value.append(0.0)
-                    else:
-                        feat_idx.append(feat_dict_[features[idx]])
-                        feat_value.append(1.0)
-
-                features_idxs.append(feat_idx)
-                features_values.append(feat_value)
-                labels.append([int(features[0])])
-    features_idxs = np.array(features_idxs)
-    features_values = np.array(features_values)
-    labels = np.array(labels).astype(np.int32)
-    return features_idxs, features_values, labels
-
-
-class CriteoDataset(Dataset):
-    """
-    这种方式由于速度过慢, 已经弃用
-    """
-    def __init__(self, filelist):
-        file_idxs = [int(re.sub('[\D]', '', x)) for x in filelist]
-        self.filelist = [filelist[idx] for idx in np.argsort(file_idxs)]
-        self.each_file_item_num = EACH_FILE_DATA_NUM
-        item_count = 0
-        for fname in filelist:
-            with open(fname.strip(), 'r') as fin:
-                for _ in fin:
-                    item_count += 1
-        self.item_count = item_count
-
-        self.continuous_range_ = range(1, 14)
-        self.categorical_range_ = range(14, 40)
-        self.cont_min_ = [0, -3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        self.cont_max_ = [5775, 257675, 65535, 969, 23159456, 431037, 56311, 6047, 29019, 46, 231, 4008, 7393]
-        self.cont_diff_ = [self.cont_max_[i] - self.cont_min_[i] for i in range(len(self.cont_min_))]
-        self.feat_dict_ = pickle.load(open('data/aid_data/feat_dict_10.pkl2', 'rb'))
-
-    def __getitem__(self, idx):
-        # 根据idx得到它所在的下标
-        file_idx = int(idx / self.each_file_item_num)
-        item_idx = idx % self.each_file_item_num
-
-        feat_idx = []
-        feat_value = []
-        label = []
-
-        fname = self.filelist[file_idx]
-        with open(fname.strip(), 'r') as fin:
-            count = 0
-            for line_idx, line in enumerate(fin):
-                count += 1
-                if line_idx == item_idx:
-                    features = line.rstrip('\n').split('\t')
-
-                    # MinMax标准化连续型数据
-                    for idx in self.continuous_range_:
-                        if features[idx] == '':
-                            feat_idx.append(0)
-                            feat_value.append(0.0)
-                        else:
-                            feat_idx.append(self.feat_dict_[idx])
-                            feat_value.append((float(features[idx]) - self.cont_min_[idx - 1]) / self.cont_diff_[idx - 1])
-
-                    #
-                    for idx in self.categorical_range_:
-                        if features[idx] == '' or features[idx] not in self.feat_dict_:
-                            feat_idx.append(0)
-                            feat_value.append(0.0)
-                        else:
-                            feat_idx.append(self.feat_dict_[features[idx]])
-                            feat_value.append(1.0)
-
-                    label.append(int(features[0]))
-        return np.array(feat_idx), np.array(feat_value), np.array(label).astype(np.int32)
-
-    def __len__(self):
-        return self.item_count
+# """ ************************************************************************************ """
+# """                             The following two func are out of used                   """
+# """ ************************************************************************************ """
+# def get_criteo_data(filelist):
+#     """
+#     get the criteo data in filelist
+#     :param filelist:
+#     :return:
+#     """
+#     continuous_range_ = range(1, 14)
+#     categorical_range_ = range(14, 40)
+#     cont_min_ = [0, -3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+#     cont_max_ = [5775, 257675, 65535, 969, 23159456, 431037, 56311, 6047, 29019, 46, 231, 4008, 7393]
+#     cont_diff_ = [cont_max_[i] - cont_min_[i] for i in range(len(cont_min_))]
+#     print(os.getcwd())
+#     feat_dict_ = pickle.load(open('data/aid_data/feat_dict_10.pkl2', 'rb'))
+#
+#     count = 0
+#     features_idxs, features_values, labels = [], [], []
+#     for fname in filelist:
+#
+#         count += 1
+#         print(count)
+#
+#         with open(fname.strip(), 'r') as fin:
+#             for line in fin:
+#                 features = line.rstrip('\n').split('\t')
+#                 feat_idx = []
+#                 feat_value = []
+#
+#                 # MinMax Normalization
+#                 for idx in continuous_range_:
+#                     if features[idx] == '':
+#                         feat_idx.append(0)
+#                         feat_value.append(0.0)
+#                     else:
+#                         feat_idx.append(feat_dict_[idx])
+#                         feat_value.append((float(features[idx]) - cont_min_[idx - 1]) / cont_diff_[idx - 1])
+#
+#                 #
+#                 for idx in categorical_range_:
+#                     if features[idx] == '' or features[idx] not in feat_dict_:
+#                         feat_idx.append(0)
+#                         feat_value.append(0.0)
+#                     else:
+#                         feat_idx.append(feat_dict_[features[idx]])
+#                         feat_value.append(1.0)
+#
+#                 features_idxs.append(feat_idx)
+#                 features_values.append(feat_value)
+#                 labels.append([int(features[0])])
+#     features_idxs = np.array(features_idxs)
+#     features_values = np.array(features_values)
+#     labels = np.array(labels).astype(np.int32)
+#     return features_idxs, features_values, labels
+#
+#
+# class CriteoDataset(Dataset):
+#     """
+#     Warning: This function is Too Slow(Not recommended)
+#     """
+#     def __init__(self, filelist):
+#         file_idxs = [int(re.sub('[\D]', '', x)) for x in filelist]
+#         self.filelist = [filelist[idx] for idx in np.argsort(file_idxs)]
+#         self.each_file_item_num = EACH_FILE_DATA_NUM
+#         item_count = 0
+#         for fname in filelist:
+#             with open(fname.strip(), 'r') as fin:
+#                 for _ in fin:
+#                     item_count += 1
+#         self.item_count = item_count
+#
+#         self.continuous_range_ = range(1, 14)
+#         self.categorical_range_ = range(14, 40)
+#         self.cont_min_ = [0, -3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+#         self.cont_max_ = [5775, 257675, 65535, 969, 23159456, 431037, 56311, 6047, 29019, 46, 231, 4008, 7393]
+#         self.cont_diff_ = [self.cont_max_[i] - self.cont_min_[i] for i in range(len(self.cont_min_))]
+#         self.feat_dict_ = pickle.load(open('data/aid_data/feat_dict_10.pkl2', 'rb'))
+#
+#     def __getitem__(self, idx):
+#         file_idx = int(idx / self.each_file_item_num)
+#         item_idx = idx % self.each_file_item_num
+#
+#         feat_idx = []
+#         feat_value = []
+#         label = []
+#
+#         fname = self.filelist[file_idx]
+#         with open(fname.strip(), 'r') as fin:
+#             count = 0
+#             for line_idx, line in enumerate(fin):
+#                 count += 1
+#                 if line_idx == item_idx:
+#                     features = line.rstrip('\n').split('\t')
+#
+#                     # MinMax Normalization
+#                     for idx in self.continuous_range_:
+#                         if features[idx] == '':
+#                             feat_idx.append(0)
+#                             feat_value.append(0.0)
+#                         else:
+#                             feat_idx.append(self.feat_dict_[idx])
+#                             feat_value.append((float(features[idx]) - self.cont_min_[idx - 1]) / self.cont_diff_[idx - 1])
+#
+#                     #
+#                     for idx in self.categorical_range_:
+#                         if features[idx] == '' or features[idx] not in self.feat_dict_:
+#                             feat_idx.append(0)
+#                             feat_value.append(0.0)
+#                         else:
+#                             feat_idx.append(self.feat_dict_[features[idx]])
+#                             feat_value.append(1.0)
+#
+#                     label.append(int(features[0]))
+#         return np.array(feat_idx), np.array(feat_value), np.array(label).astype(np.int32)
+#
+#     def __len__(self):
+#         return self.item_count
 
 
 if __name__ == '__main__':
